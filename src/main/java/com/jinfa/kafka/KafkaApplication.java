64944ca526b5e5838ca04471b4e060509ffcc8ca
jinfa <312904843@qq.com>
2018-12-16 21:05:03 +0800

    msg

--- src/main/java/com/jinfa/kafka/KafkaApplication.java
+++ src/main/java/com/jinfa/kafka/KafkaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KafkaApplication {
 
     public static void main(String[] args) {
+        //fuck you song
         SpringApplication.run(KafkaApplication.class, args);
     }
 
